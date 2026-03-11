e15bedc4e869dc71b0ab7e89c0c547caa83982c2
caiofeitosadev <devcaioeduardo@gmail.com>
2026-03-11 04:39:54 -0300

    refactor(auth): centralize NextAuth configuration in lib/auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import { authOptions } from '@/lib/auth';
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          prompt: 'select_account',
-          acess_type: 'offline',
-          response_type: 'code',
-        },
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string,
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,20 @@
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 export const metadata = {
   title: 'Dashboard',
 };
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/');
+  }
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>Welcome, {session.user?.name}</p>
     </div>
   );
 }
