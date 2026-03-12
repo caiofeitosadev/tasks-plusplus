dcf1660af3d13596b3319bcd6f39ab5be4195ef5
caiofeitosadev <devcaioeduardo@gmail.com>
2026-03-12 07:32:07 -0300

    refactor: move firebase config to environment variables

--- lib/firebaseConnection.ts
+++ lib/firebaseConnection.ts
@@ -1,18 +1,17 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBghu3zrcXW0qxrpNbXz2QJGDFbHPqaIyw',
-  authDomain: 'tasksplusplus-11df3.firebaseapp.com',
-  projectId: 'tasksplusplus-11df3',
-  storageBucket: 'tasksplusplus-11df3.firebasestorage.app',
-  messagingSenderId: '899593345163',
-  appId: '1:899593345163:web:59338d94f6dfe353879c8e',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-const db = getFirestore(firebaseApp);
+const db = getFirestore(app);
 
 export { db };
